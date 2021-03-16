e1879992d2d3aa49cf9bfb9ec7888245cf4cdcf6
Petr Máčal <czhakis@gmail.com>
2021-03-16 16:03:10 +0100

    antd

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import './index.css'
+import 'antd/dist/antd.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { cache  } from './graphql/cache'
