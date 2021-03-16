7fcae4f1ed32f14c8c6b1efa1a4b381fa94789d2
Petr Máčal <czhakis@gmail.com>
2021-03-16 14:33:00 +0100

    uuid4, add ids to schema

--- server/src/defs.ts
+++ server/src/defs.ts
@@ -2,17 +2,20 @@ import { gql } from 'apollo-server'
 
 export const TypeDefs = gql`
   type Card {
+    id: ID
     label: String
     sections: [Section]
   }
 
   type Section {
+    id: ID
     label: String
     weight: Int
     items: [Item]
   }
 
   type Item {
+    id: ID
     label: String
     status: Status
   }
