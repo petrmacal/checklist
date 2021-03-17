fdaccadfeb7279395b8b3a515ad8e4b13c63efb2
Petr Máčal <czhakis@gmail.com>
2021-03-17 01:07:06 +0100

    Quote component

---
+++ client/src/components/Quote.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+
+export default function Quote() {
+  const [quote, setQuote] = useState('')
+  useEffect(() => {
+    fetch('https://uselessfacts.jsph.pl/random.json')
+      .then(res => res.json())
+      .then((res) => setQuote(res.text))
+      .catch(() => setQuote('I would really like to show some cool quote here, but I have probably reached the limit of this API :('))
+  })
+
+  return (
+    <div>{quote}</div>
+  )
+}
