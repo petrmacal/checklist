50f3e13664be4981e0b9432578bf5868f92bb1eb
Petr Máčal <czhakis@gmail.com>
2021-03-17 01:08:02 +0100

    Progress

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import { Layout, Card, Row, Col, Progress, Avatar } from 'antd'
 import { gql , useQuery} from '@apollo/client'
 import { FetchCards } from './__generated__/FetchCards'
 import ListItem from './components/ListItem'
+import Quote from './components/Quote'
+import CreateStartup from './components/CreateStartup'
 
 const { Content, Footer } = Layout
 
@@ -17,6 +19,7 @@ const FETCH_CARDS = gql`
         items {
           id
           label
+          status
         }
       }
     }
@@ -29,24 +32,42 @@ const Cards: React.FC<any> = () => {
   if (loading) return <Progress />
   if (error || !data) return <p>ERROR</p>
 
+  const modifiedData = data.fetchCards?.map(({ sections, ...rest }) => {
+    return { ...rest, sections: sections.map((section, i) => ({
+      ...section,
+      eligible: i > 0 ? sections[i-1].items.every(task => task.status) : true,
+      done: section.items.every(task => task.status)
+    })) }
+  }) || []
+
   return (
-    <Row gutter={12}>
-      {data && data.fetchCards && data.fetchCards.map(card => (
+    <Row gutter={24}>
+      {modifiedData.map(card => (
         <Col key={card.id}>
           <Card>
             <h2>{card.label}</h2>
-            {card.sections.map((section, i) => (
+            {card.sections.map((section, i, sections) => (
               <React.Fragment key={section.id}>
                 <div style={{display: 'flex', alignItems: 'center'}}>
-                  <Avatar>{i + 1}</Avatar>
+                  <Avatar
+                    style={{backgroundColor: section.done && section.eligible && '#000000' || '#aaaaaa', transition: '.5s all'}}>
+                      {i + 1}
+                  </Avatar>
                   <h3>{section.label}</h3>
                 </div>
-                {section.items.map(task => <ListItem key={task.id} label={task.label} />)}
+                {section.items.map(task => <ListItem key={task.id} cardId={card.id} eligible={sections.slice(0, i).every(s => s.eligible) && section.eligible} task={task} />)}
               </React.Fragment>
             ))}
+            {card.sections.every(section => section.done) && (
+              <Quote />
+            )}
           </Card>
         </Col>
       ))}
+
+      {data && data.fetchCards?.length === 0 && (
+        <CreateStartup />
+      )}
     </Row>
   )
 }
